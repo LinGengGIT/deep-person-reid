49d46d7e81815338fc61e2f41b9d117f1f1fc214
gyxoned <geyixiao831@gmail.com>
2018-05-09 01:24:39 +0800

    add test script

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-python train_qan.py --save-dir log/qan-res50-ilidsvid \
+python main.py --save-dir log/qan-res50-ilidsvid \
 	-b 32 --gpu-devices 0,1,2,3 --pool qan \
 	--optim adam --lr 0.0003 \
 	--max-epoch 200 --stepsize 100 --eval-step 50 --print-freq 4
