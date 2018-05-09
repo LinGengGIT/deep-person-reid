ea67ad55332e917975300fa7e23b8724c8138f70
yxgeee <geyixiao831@gmail.com>
2018-05-09 13:47:14 +0800

    update baseline for seq

--- scripts/train_baseline.sh
+++ scripts/train_baseline.sh
@@ -1,4 +1,4 @@
-python baseline.py --save-dir log/baseline-res50-ilidsvid \
-	-b 32 --gpu-devices 4,5,6,7 \
+python baseline.py --save-dir log/baseline-res50-ilidsvid-eraser \
+	-b 32 --gpu-devices 4,5,7 \
 	--optim adam --lr 0.0003 \
-	--max-epoch 200 --stepsize 100 --eval-step 50 --print-freq 4
+	--max-epoch 500 --stepsize 200 --eval-step 50 --print-freq 4
