9ff59bcf20e6a11a133b37c87520dcf67d9f6b19
yxgeee <geyixiao831@gmail.com>
2018-05-09 14:44:20 +0800

    update scripts

--- scripts/train_baseline.sh
+++ scripts/train_baseline.sh
@@ -1,4 +1,4 @@
-python baseline.py --save-dir log/baseline-res50-ilidsvid-eraser \
+python baseline.py --save-dir log/baseline-res50-ilidsvid \
 	-b 32 --gpu-devices 4,5,7 \
 	--optim adam --lr 0.0003 \
 	--max-epoch 500 --stepsize 200 --eval-step 50 --print-freq 4
--- scripts/train_qan.sh
+++ scripts/train_qan.sh
@@ -1,4 +1,4 @@
 python qan.py --save-dir log/qan-res50-ilidsvid \
 	-b 32 --gpu-devices 0,1,2,3 --pool qan \
 	--optim adam --lr 0.0003 \
-	--max-epoch 200 --stepsize 100 --eval-step 50 --print-freq 4
+	--max-epoch 300 --stepsize 200 --eval-step 50 --print-freq 4
